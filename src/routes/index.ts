362d8bdf33f070642e594512dd457d5811bb0b02
Robin Dittmar <robindittmar@gmail.com>
2023-10-15 18:24:15 +0200

    Removed unnecessary data fetching for server side rendering

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,9 @@
 import express from 'express';
-import {getCurrentBalance} from "../services/transaction-service";
 
 export const indexRouter = express.Router();
 
 
 indexRouter.get('/', async (_req, res) => {
-    let data = await getCurrentBalance();
-
-    res.render('index', {title: 'cantropee', data});
+    res.render('index', {title: 'cantropee'});
 });
 
