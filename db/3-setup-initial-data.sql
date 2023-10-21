a6de35cfb872fb9442d3e4305289cdf9d915a889
Robin Dittmar <robindittmar@gmail.com>
2023-10-21 22:27:26 +0200

    * fixed label in login form * fixed sql migration script

--- db/3-setup-initial-data.sql
+++ db/3-setup-initial-data.sql
@@ -11,6 +11,5 @@ INSERT INTO cantropee.organization_users (organization_id, user_id, role)
 VALUES (UUID_TO_BIN('6ba780d2-7026-11ee-bc40-0242ac110002'), UUID_TO_BIN('712d61c4-7026-11ee-bc40-0242ac110002'),
         1);
 
-INSERT INTO cantropee.categories (id, name)
-VALUES (1, 'Nicht spezifiziert');
-
+INSERT INTO cantropee.categories (id, organization_id, name)
+VALUES (1, UUID_TO_BIN('6ba780d2-7026-11ee-bc40-0242ac110002'), 'Nicht spezifiziert');
