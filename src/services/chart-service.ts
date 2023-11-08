af30c1e92da0196a0f566a35d613934ba9d569b2
Robin Dittmar <robindittmar@gmail.com>
2023-11-08 22:36:15 +0100

    started working on a chart service

---
+++ src/services/chart-service.ts
@@ -0,0 +1,54 @@
+import {getTransactions} from "./transaction-service";
+
+interface ChartData {
+    [key: number]: number;
+}
+
+export async function getChart(organizationId: string) {
+    const now = new Date();
+    const startDate = new Date();
+    startDate.setFullYear(startDate.getFullYear() - 1);
+
+    const transactions = await getTransactions(
+        organizationId,
+        startDate,
+        now,
+        0,
+        100000000,
+        true,
+        undefined,
+    );
+
+    let values: ChartData = {};
+    let currentTotal = 0;
+    let currentMonth: number | undefined = undefined;
+    for (const t of transactions.data) {
+        if (currentMonth === undefined) {
+            currentMonth = t.effectiveTimestamp.getMonth();
+
+            console.log('1st month: ' + currentMonth);
+        }
+        if (t.effectiveTimestamp.getMonth() != currentMonth) {
+            values[currentMonth + 1] = currentTotal;
+            currentTotal = 0;
+            currentMonth = t.effectiveTimestamp.getMonth();
+
+            console.log('next month: ' + currentMonth);
+        }
+
+        currentTotal += t.value;
+        console.log(`+ ${t.value} = ${currentTotal}`);
+    }
+
+    // We need to apply last accumulation
+    // if (t.effectiveTimestamp.getMonth() != currentMonth) {
+    //     values[currentMonth + 1] = currentTotal;
+    //     currentTotal = 0;
+    //     currentMonth = t.effectiveTimestamp.getMonth();
+    //
+    //     console.log('next month: ' + currentMonth);
+    // }
+
+    console.log(values);
+    return values;
+}
