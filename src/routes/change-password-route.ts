d8a8b9d3885cc137ad1156e89a7d1391d49598d7
Robin Dittmar <robindittmar@gmail.com>
2023-12-12 14:24:22 +0100

    removed 'confirmPassword' parameter from update password endpoint. Frontend should validate this

--- src/routes/change-password-route.ts
+++ src/routes/change-password-route.ts
@@ -10,20 +10,12 @@ changePasswordRouter.post('/', async (req, res, next) => {
     try {
         const session = getSessionFromReq(req);
 
-        const {password, confirmPassword} = req.body;
-
-        if (!password) {
-            throw new ServerError(400, 'field "password" is missing in request body');
-        }
-        if (!confirmPassword) {
-            throw new ServerError(400, 'field "confirmPassword" is missing in request body');
+        const {password} = req.body;
+        if (!password || typeof password !== 'string') {
+            throw new ServerError(400, 'field "password" is missing or not a string');
         }
 
-        if (password !== confirmPassword) {
-            throw new ServerError(400, 'passwords do not match');
-        }
-
-        let success = await updateUserPassword(session.user, req.body.password);
+        let success = await updateUserPassword(session.user, password);
         if (!success) {
             res.status(400);
         }
